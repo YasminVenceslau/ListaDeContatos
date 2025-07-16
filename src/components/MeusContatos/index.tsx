b15c47b2fd09c4b0092d2eddfe48983f4dcb1c81
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-07-15 23:47:33 -0300

    Update index.tsx

--- src/components/MeusContatos/index.tsx
+++ src/components/MeusContatos/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type ChangeEvent } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { remover, editar, alteraStatus } from '../../store/reducers/tarefas'
